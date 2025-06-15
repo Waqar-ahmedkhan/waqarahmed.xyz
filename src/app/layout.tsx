d9cf96cdb3feafbbe032cb320afb2e76b536c5c8
Waqar ahmed <waqarahmed44870@gmail.com>
2025-06-15 08:37:59 +0500

    add gtm

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -80,24 +80,32 @@ export default function RootLayout({
 }) {
   return (
     <html lang="en" suppressHydrationWarning>
-      <head />
-      <body className={`${inter.className} bg-white text-black dark:bg-zinc-900 dark:text-white transition-colors`}>
-        <AppThemeProvider>
-          {children}
-          {/* Google Analytics */}
-          <Script
-            async
-            src="https://www.googletagmanager.com/gtag/js?id=G-XXXXXXX"
-          />
-          <Script id="google-analytics">
-            {`
-              window.dataLayer = window.dataLayer || [];
-              function gtag(){dataLayer.push(arguments);}
-              gtag('js', new Date());
-              gtag('config', 'G-XXXXXXX');
-            `}
-          </Script>
-        </AppThemeProvider>
+      <head>
+        {/* Google Tag Manager - Head */}
+        <Script id="gtm-script" strategy="afterInteractive">
+          {`
+            (function(w,d,s,l,i){w[l]=w[l]||[];w[l].push({'gtm.start':
+            new Date().getTime(),event:'gtm.js'});var f=d.getElementsByTagName(s)[0],
+            j=d.createElement(s),dl=l!='dataLayer'?'&l='+l:'';j.async=true;j.src=
+            'https://www.googletagmanager.com/gtm.js?id='+i+dl;f.parentNode.insertBefore(j,f);
+            })(window,document,'script','dataLayer','GTM-NPKQCQK3');
+          `}
+        </Script>
+      </head>
+      <body
+        className={`${inter.className} bg-white text-black dark:bg-zinc-900 dark:text-white transition-colors`}
+      >
+        {/* Google Tag Manager - NoScript */}
+        <noscript>
+          <iframe
+            src="https://www.googletagmanager.com/ns.html?id=GTM-NPKQCQK3"
+            height="0"
+            width="0"
+            style={{ display: "none", visibility: "hidden" }}
+          ></iframe>
+        </noscript>
+
+        <AppThemeProvider>{children}</AppThemeProvider>
       </body>
     </html>
   );
